7b7e055cb3fa6a99624d3a65e7d9598231e28f12
Meena <rgmeena1404@gmail.com>
2020-05-12 13:00:31 +0200

    Updates

---
+++ precache-manifest.8fb3166d4fa685dfc5c3615635755aa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15e5d86d2780f0dcbb394ebd64ca023",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0c24677ef551cf3e5814",
+    "url": "/portfolio/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "8e12ef741196337c5577",
+    "url": "/portfolio/static/css/main.2b1b4a7e.chunk.css"
+  },
+  {
+    "revision": "0c24677ef551cf3e5814",
+    "url": "/portfolio/static/js/2.7b030b3d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.7b030b3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e12ef741196337c5577",
+    "url": "/portfolio/static/js/main.7e9e03f9.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
